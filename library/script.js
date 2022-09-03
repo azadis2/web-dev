c14133bf43227b3c9d9922a9c96c2b406a6103be
Sarvin <azadis2@mcmaster.ca>
2022-09-03 10:49:47 -0400

    finished admin dashboard project

--- library/script.js
+++ library/script.js
@@ -1,22 +1,3 @@
-let myLibrary = [];
-let add = document.querySelector('p');
-const form = document.querySelector('.form-container');
-const addbtn = document.getElementById('add');
-const submit = document.querySelector('submit');
-// don't show the form by default
-form.style.display = 'none';
-
-addbtn.addEventListener('click', ()=> {
-    form.style.display = 'flex';
-})
-
-submit.addEventListener('click', (title, author, pages, read) => {
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    display(myLibrary);
-}
-)
-
 // book function constructor
 function Book(title, author, pages, read) {
     this.title = title;
@@ -24,14 +5,52 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+let myLibrary = [];
+let add = document.querySelector('p');
+let container = document.querySelector('.container');
+let small = document.querySelector('.small-container');
+const form = document.querySelector('.form-container');
+const addbtn = document.getElementById('add');
+const submit = document.querySelector('.submit');
+// don't show the form by default
+form.style.display = 'none';
+
+addbtn.addEventListener('click', ()=> {
+    form.style.display = 'block';
+})
+
+submit.addEventListener('click', (title, author, pages, read) => {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    display(myLibrary);
+    // stop page from refreshing when submitting form
+    document.getElementById('theForm').addEventListener('submit', handleForm);
+    // making form disappear when it was submitted
+    form.style.display = 'none';
+    
+}
+)
+// function to stop form from refreshing
+function handleForm(event) { event.preventDefault(); } 
+
+
 
 // a function that loops through myLibrary and displays each book
 function display(myLibrary) {
-    myLibrary.forEach(function(book) {
+    myLibrary.forEach(book => {
         let newDiv = document.createElement('div');
-        newDiv.style.display = 'block';
-        newDiv.innerHTML = '<p>${book.title}</p> <p>${book.author}</p> <p>${book.pages}</p> <p>${book.read}</p>'
-        newDiv.style.color='#222c3c';
+        let newBtn = document.createElement('button');
+        newDiv.textContent = '<p>' + book.title + '</p>' + '<p>'+ book.author + '</p>' + '<p>' + book.pages + '</p>' + '<p>' + book.read + '</p>' ;
+        newDiv.style.backgroundColor = '#ECD444';
+        newDiv.style.color = 'black'
+        newDiv.style.width = '250px';
+        newDiv.style.borderRadius = '10px';
+        newDiv.style.display = 'flex';
+        newDiv.style.flexDirection = 'column';
+        newDiv.style.alignItems= 'center';
+        newDiv.style.justifyContent = 'space-between';
+        newDiv.appendChild(newBtn);
+        small.appendChild(newDiv);
     })
+    
 }
-
